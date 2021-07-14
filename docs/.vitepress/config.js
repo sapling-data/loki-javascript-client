02e9a7eb96068f44b51eb1b26bc3f31f2fb50acb
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2021-07-14 00:02:34 -0500

    Update Vitepress config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,19 @@
+const introduction = [
+  { text: 'What is Loki JavaScript Client?', link: '/introduction/what-is-loki-javascript-client' },
+  { text: 'Get started', link: '/introduction/get-started' }
+]
+
 module.exports = {
   title: 'Loki JavaScript Client',
-  description: 'Testing.'
+  description: 'Testing.',
+  themeConfig: {
+    sidebar: {
+      '/': [
+        {
+          text: "Introduction",
+          children: introduction
+        },
+      ],
+    } 
+  }
 }
